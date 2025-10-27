9dc8b3db9ba3e1bb4a0a0ef14fab5c274d5a5f43
AP <adonis.paz@grupobusiness.it>
2025-10-27 18:04:26 -0500

    paso

--- app.py
+++ app.py
@@ -7,5 +7,27 @@ app = Flask(__name__)
 def hello_world():
     return 'Hola desde app B', 200
 
+@app.route('/health', methods=['GET'])
+def call_app_b():
+    try:
+        # Llamamos a la app B, que está en localhost:3001
+        return 'OK', response.status_code
+    except requests.exceptions.ConnectionError:
+        return 'Error: no se pudo conectar con app B', 500
+@app.route('/startup', methods=['GET'])
+def call_app_b():
+    try:
+        # Llamamos a la app B, que está en localhost:3001
+        return 'OK', response.status_code
+    except requests.exceptions.ConnectionError:
+        return 'Error: no se pudo conectar con app B', 500
+@app.route('/readiness', methods=['GET'])
+def call_app_b():
+    try:
+        # Llamamos a la app B, que está en localhost:3001
+        return 'OK', response.status_code
+    except requests.exceptions.ConnectionError:
+        return 'Error: no se pudo conectar con app B', 500
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
